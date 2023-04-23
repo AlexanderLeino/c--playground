c90b0eb07e0e87e87ab63b85cc4daa103652005b
Alexander Leino <Alexander.Leino@yahoo.com>
2023-04-23 00:12:48 -0400

    started Derek Banas C# tutorial for beginners

--- Program.cs
+++ Program.cs
@@ -1,58 +1,30 @@
-﻿public class Functions
+﻿using System;
+namespace ConsoleApp1 
 {
-    public static long Factorial(int n)
+    public class Program
     {
-        // Test for invalid input.
-        if ((n < 0) || (n > 20))
+        //Void means that it will not return anything after executing!
+        static void Main(string[] args) 
         {
-            return -1;
-        }
+            string randString = "This is a string"; 
+            Console.WriteLine("String Length : {0}", randString.Length);
+            Console.WriteLine("String Contains is : {0}", randString.Contains("is"));
+            Console.WriteLine("Index of is : {0}", randString.IndexOf("is"));
+            Console.WriteLine("Remove : {0}", randString.Remove(10, 6));
+            Console.WriteLine("Insert String: {0}", randString.Insert(10, "Short "));
+            Console.WriteLine("Replace String : {0}", randString.Replace("string", "sentence"));
 
-        // Calculate the factorial iteratively rather than recursively.
-        long tempResult = 1;
-        for (int i = 1; i <= n; i++)
-        {
-            tempResult *= i;
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("A = a : {0}", String.Equals("A", "a", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine("Pad Left : {0}", randString.PadLeft(20, '.'));
+            Console.WriteLine("Pad Right : {0}", randString.PadRight(20, '.'));
+            Console.WriteLine("Trim Left : {0}", randString.Trim());
+            Console.WriteLine("Uppercase : {0}", randString.ToUpper());
+            Console.WriteLine("Lowercase: {0}", randString.ToLower());
+            string newString = String.Format("{0} saw a {1} {2} in the {3}", "Paul", "rabbit", "eating", "Corn");
+            Console.Write(newString + "\n");
+            Console.WriteLine(@"Exactly What I Typed\n");
+        
         }
-        return tempResult;
     }
 }
-
-class MainClass
-{
-    static int Main(string[] args)
-    {
-        // Test if input arguments were supplied.
-        if (args.Length == 0)
-        {
-            Console.WriteLine("Please enter a numeric argument.");
-            Console.WriteLine("Usage: Factorial <num>");
-            return 1;
-        }
-
-        // Try to convert the input arguments to numbers. This will throw
-        // an exception if the argument is not a number.
-        // num = int.Parse(args[0]);
-        int num;
-        bool test = int.TryParse(args[0], out num);
-        if (!test)
-        {
-            Console.WriteLine("Please enter a numeric argument.");
-            Console.WriteLine("Usage: Factorial <num>");
-            return 1;
-        }
-
-        // Calculate factorial.
-        long result = Functions.Factorial(num);
-
-        // Print result.
-        if (result == -1)
-            Console.WriteLine("Input must be >= 0 and <= 20.");
-        else
-            Console.WriteLine($"The Factorial of {num} is {result}.");
-
-        return 0;
-    }
-}
-// If 3 is entered on command line, the
-// output reads: The factorial of 3 is 6.
